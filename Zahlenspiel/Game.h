a7e3ce98d24e5ad55fb53658326f614ac532fd54
Julia <Kammerlander@ztt.fh-worms.de>
2014-02-08 12:05:50 +0100

    zu #1

---
+++ Zahlenspiel/Game.h
@@ -0,0 +1,24 @@
+#pragma once //strg+leer = autoergänzung; strg+. = lösungsvorschlag
+
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+
+using namespace std;
+
+class Game
+{
+	int random;
+	int guest; //variablennamen s. Frank
+	int numberoftries;
+	void set_random();
+	void set_guest();
+
+	void round();
+public:
+	Game(void);
+	~Game(void);
+
+		int turn();
+};
+
