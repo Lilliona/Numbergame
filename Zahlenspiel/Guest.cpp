10d737498595d83fdda8510d537f38005e1cfd27
Julia <Kammerlander@ztt.fh-worms.de>
2014-02-10 16:48:05 +0100

    zu #6

--- Zahlenspiel/Guest.cpp
+++ Zahlenspiel/Guest.cpp
@@ -12,6 +12,9 @@ void Guest::set_choosenumber()
 {
 	cout << "Please insert a value between 0 and 100: ";
 	cin >> choosenumber;
+
+//	if(choosenumber=="A" || "B"....) aber mehrstellige?
+		cout << ""<< endl;
 }
 
 int Guest::get_choosenumber()
