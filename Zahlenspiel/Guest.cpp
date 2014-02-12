25b4a9158f5cc75d5703adeed7cbb6133328149d
Julia <kammerlander@ztt.fh-worms.de>
2014-02-12 13:28:42 +0100

    zu #7

--- Zahlenspiel/Guest.cpp
+++ Zahlenspiel/Guest.cpp
@@ -10,11 +10,47 @@ Guest::~Guest(void)
 
 void Guest::set_choosenumber()
 {
-	cout << "Please insert a value between 0 and 100: ";
-	cin >> choosenumber;
 
-//	if(choosenumber=="A" || "B"....) aber mehrstellige?
-		cout << ""<< endl;
+	cout << "Please enter a value between 0 and 100: ";
+
+	string isNumeric;
+
+	while (1)
+	{
+		getline(cin, isNumeric);
+
+		bool validateNumeric = true;
+
+		for (unsigned i=0; i < isNumeric.length(); i++)
+		{
+			if (!isdigit(isNumeric[i]))
+			{
+				validateNumeric = false;
+				break;
+			}
+		}
+		if(!validateNumeric)
+		{
+			cout << "wrong entry. try again: ";
+			continue; // Springt zu Schleifenanfang
+		}
+
+		stringstream sStream (isNumeric);
+		sStream >> choosenumber; 
+
+		if(choosenumber>100)
+		{
+			cout << "You are over the valid range. Try again: ";
+			continue;
+		}
+
+		if(choosenumber<0)
+		{
+			cout << "You are under the valid range. Try again: ";
+			continue;
+		}
+		break;
+	}
 }
 
 int Guest::get_choosenumber()
